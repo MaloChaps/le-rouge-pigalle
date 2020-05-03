b3e1e9d01fffe95c5b9bb85be157c1c506caa8d5
Malo Chaperon <chaperon.malo@gmail.com>
2020-05-03 18:14:18 +0200

    all

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 $(window).load(function() {
-     // $(".loader").fadeOut("1000"); 
+     $(".loader").fadeOut("1000"); 
  });
 
 $(document).ready(function(){
@@ -22,19 +22,6 @@ $(document).ready(function(){
                     $('.nav-open-mobile').fadeOut('1000');
                     $('.nav-mobile-close-mobile').fadeIn('1000');
                 });
-
-                //  $('#nav-open-mobile').click(function(){
-                //     $('.nav-mobile-close-mobile').css('display', 'none');
-                //     $('.nav-open-mobile').css('display', 'flex');
-                // });
-
-                // $('#nav-close-mobile').click(function(){
-                //     $('.nav-open-mobile').css('display', 'none');
-                //     $('.nav-mobile-close-mobile').css('display', 'flex');
-                // });
-
-
-                
                 
 
             });
@@ -48,5 +35,107 @@ const parallax = document.querySelectorAll("#parallax");
                 prllx.style.backgroundPositionY = (offset - prllx.offsetTop) * 0.8 + "px";
             })
         });
+
+
+// scroll reveal anime
+const sr = ScrollReveal();
+
+sr.reveal('#logo-hero', {
+	 delay: 0,
+    duration: 2000,
+    distance: "150px",
+    origin: "bottom"
+});
+
+sr.reveal('#title-upcoming', {
+	 delay: 0.2,
+    duration: 2000,
+    distance: "150px",
+    origin: "bottom"
+});
+
+sr.reveal('#line-upcoming', {
+	 delay: 0.2,
+    duration: 2000,
+    distance: "150px",
+    origin: "bottom"
+});
+
+
+sr.reveal('#subtitle-upcoming', {
+	 delay: 0.2,
+    duration: 2000,
+    distance: "150px",
+    origin: "bottom"
+});
+
+sr.reveal('#paragraphe-upcoming', {
+	 delay: 0.2,
+    duration: 2000,
+    distance: "150px",
+    origin: "bottom"
+});
+
+sr.reveal('#btn-upcoming', {
+	 delay: 0.2,
+    duration: 2000,
+    distance: "150px",
+    origin: "bottom"
+});
+
+
+sr.reveal('.video-section', {
+	 delay: 0.2,
+    duration: 2000,
+    distance: "150px",
+    origin: "bottom"
+});
+
+sr.reveal('.cover-section', {
+	 delay: 0.2,
+    duration: 2000,
+    distance: "150px",
+    origin: "bottom"
+});
+
+sr.reveal('.love-section', {
+	 delay: 0.2,
+    duration: 2000,
+    distance: "150px",
+    origin: "bottom"
+});
+
+sr.reveal('.audit-section', {
+	 delay: 0.2,
+    duration: 2000,
+    distance: "150px",
+    origin: "bottom"
+});
+
+sr.reveal('.red-section', {
+	 delay: 0.2,
+    duration: 2000,
+    distance: "150px",
+    origin: "bottom"
+});
+
+sr.reveal('.friends-section', {
+	 delay: 0.2,
+    duration: 2000,
+    distance: "150px",
+    origin: "bottom"
+});
+
+sr.reveal('footer', {
+	 delay: 0.2,
+    duration: 2000,
+    distance: "0px",
+    origin: "bottom"
+});
+
+
+
+
+
 
 
