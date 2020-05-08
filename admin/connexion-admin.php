56e6baceaca004d2a67e8496dcda2aa8b2c5b75c
Malo Chaperon <chaperon.malo@gmail.com>
2020-05-08 10:50:09 +0200

    mm

--- admin/connexion-admin.php
+++ admin/connexion-admin.php
@@ -1,8 +1,5 @@
 <?php 
-$bdd = new PDO('mysql:host=localhost;dbname=lerouge;charset=utf8', 'root', '');
-
-// $bdd = new PDO('mysql:host=kbworldwnvmalo.mysql.db;dbname=kbworldwnvmalo;charset=utf8', 'kbworldwnvmalo', 'Arcoi600');
-
+include('../connexion.php');
  ?>
 <!DOCTYPE html>
 <html>
--- admin/inscription.php
+++ admin/inscription.php
@@ -1,7 +1,6 @@
 <?php 
 session_start(); 
-$bdd = new PDO('mysql:host=localhost;dbname=lerouge;charset=utf8', 'root', '');
-// $bdd = new PDO('mysql:host=kbworldwnvmalo.mysql.db;dbname=kbworldwnvmalo;charset=utf8', 'kbworldwnvmalo', 'Arcoi600');
+include('../connexion.php');
 if (isset($_SESSION['id'])) { ?>
 <!DOCTYPE html>
 <html>
